0de1373200b1988127c1503a904e2eb7b3e0eae1
Bernard Cesarz <cesarzb@protonmail.com>
2026-01-13 11:49:06 +0100

    Solved task #2

---
+++ two_sum.py
@@ -0,0 +1,18 @@
+"""
+Problem: Two Sum
+Link: https://neetcode.io/problems/two-integer-sum/question
+
+Given array of numbers, find a pair which sums up to a provided target.
+Result can't be the same element of an array twice.
+"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for i, num in enumerate(nums):
+            complementary = target - num
+            if complementary in seen: 
+                return sorted([seen[complementary], i])
+
+            seen[num] = i
