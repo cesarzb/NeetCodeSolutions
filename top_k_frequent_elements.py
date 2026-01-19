bb17928669c497a0bc9ceb8277ee815c8d3c9db1
Bernard Cesarz <cesarzb@protonmail.com>
2026-01-19 12:44:24 +0100

    Solution Top K Frequent Elements

---
+++ top_k_frequent_elements.py
@@ -0,0 +1,27 @@
+"""
+Problem: Top K Frequent Elements
+Link: https://neetcode.io/problems/top-k-elements-in-list/question
+
+Given an array of integers return k most frequent integers.
+"""
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freqs = {}
+        indexes = [[] for _ in range(len(nums) + 1)]
+
+        for num in nums:
+            freqs[num] = freqs.get(num, 0) + 1
+
+        for key, val in freqs.items():
+            indexes[val].append(key)
+
+        result = []
+        indexes.reverse()
+        for arr in indexes:
+            for num in arr:
+                if len(result) == k: break
+                result.append(num)
+                
+
+        return result
