bfee4ae2f8a947c59c876c1f145baae26a170354
cesarzb <cesarzb@protonmail.com>
2026-02-17 17:05:29 +0100

    Group anagrams revisited

--- group_anagrams.py
+++ group_anagrams.py
@@ -8,13 +8,11 @@ Given an array of strings group the anagrams together.
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        grouped = {}
+        anagrams = defaultdict(list)
 
         for s in strs:
-            arr = [0] * 26
+            freqs = [0] * 26
             for char in s:
-                arr[ord(char) - ord('a')] += 1
-            if tuple(arr) in grouped: grouped[tuple(arr)].append(s)
-            else: grouped[tuple(arr)] = [s]
-        
-        return list(grouped.values())
+                freqs[ord(char) - ord('a')] += 1
+            anagrams[tuple(freqs)].append(s)
+        return list(anagrams.values())
