0eda0dc877c6b0f613b2d9f10b5d5524da77cc5e
cesarzb <cesarzb@protonmail.com>
2026-04-10 13:56:57 +0200

    Revisited Two Integer Sum II

--- two_integer_sum_ii.py
+++ two_integer_sum_ii.py
@@ -9,11 +9,10 @@ You can only use O(1) space.
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        i = 0
-        j = len(numbers) - 1
+        left, right = 0, len(numbers) - 1
 
-        while i < j:
-            total = numbers[i] + numbers[j]
-            if total < target: i += 1
-            elif total > target: j -= 1
-            else: return [i + 1, j + 1]
+        while left < right:
+            total = numbers[left] + numbers[right]
+            if total < target: left += 1
+            elif total > target: right -= 1
+            else: return [left + 1, right + 1]
