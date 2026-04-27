20b0e8e84974054bc0da301834d8a22d6fae143d
cesarzb <cesarzb@protonmail.com>
2026-04-27 19:06:34 +0200

    Solved for Time Based Key-Value Store

---
+++ time_based_key_value_store.py
@@ -0,0 +1,35 @@
+"""
+Problem: Time Based Key-Value Store
+Link: https://neetcode.io/problems/time-based-key-value-store/question
+
+Implement key-value data structure that can hold values 
+for different timestamps of the same keys.
+Get method should return most recent timestamp 
+that is not newer than the provided.
+"""
+
+class TimeMap:
+
+    def __init__(self):
+        self.store = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append([timestamp, value])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        values = self.store.get(key, [])
+
+        left, right = 0, len(values) - 1
+        while left <= right:
+            mid = left + (right-left) // 2
+
+            if values[mid][0] <= timestamp:
+                res = values[mid][1]
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return res
